b2476502c51548a03b47b3ca1156475265b39a52
Claudé Davis <149217793+Claude-Davis@users.noreply.github.com>
2023-12-31 20:40:55 -0500

    Update and rename Iteration : Count Up.py.txt to Iteration : Count Up.py

---
+++ Iteration : Count Up.py
@@ -0,0 +1,8 @@
+# take a number as input
+number = 25
+x = 0
+
+#use a 'while loop' to count up
+while x <= number:             #While x is less than or equal to zero...
+    print(x)              #the value of the variable 'x' will be printed.
+    x = x + 1        #after each display of the value for 'x,' the value is increased by 1.
