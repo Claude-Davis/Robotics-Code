dc0b6a5aee4d345dbcc6435840294e842a7dca05
Claudé Davis <claudedavis2024@gmail.com>
2024-02-04 22:58:29 -0500

    Create Class Player, Destructor.cpp

---
+++ Class Player, Destructor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Player
+{
+  public:
+    int points;
+    Player(int x)
+    {
+        points = x;
+        points %= 5;
+    }
+    ~Player()
+    {
+      cout << points << endl;
+    }
+};
+int main() {
+  int points;
+  cin >> points;
+  Player obj(points);
+}
